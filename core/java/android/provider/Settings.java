b83e66ae8a5a7bf2a7edc063f8ea39ac578e2204
nift4 <nift4@protonmail.com>
2023-04-23 09:56:46 +0200

    Implement transistent task mode
    AKA "Tadi mode"
    
    Change-Id: I67a0ec743cd0084c2b63bdaf5aa78a20d269f47d

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -5727,6 +5727,11 @@ public final class Settings {
         public static final String PREVENT_POINTER_ACCELERATION = "prevent_pointer_acceleration";
 
         /**
+         * @hide
+         */
+        public static final String TRANSISTENT_TASK_MODE = "transistent_task_mode";
+
+        /**
          * Keys we no longer back up under the current schema, but want to continue to
          * process when restoring historical backup datasets.
          *
@@ -5879,6 +5884,7 @@ public final class Settings {
             PRIVATE_SETTINGS.add(GESTURE_ONE_FINGER_SWIPE_LEFT);
             PRIVATE_SETTINGS.add(FORCE_FULLSCREEN_CUTOUT_APPS);
             PRIVATE_SETTINGS.add(PREVENT_POINTER_ACCELERATION);
+            PRIVATE_SETTINGS.add(TRANSISTENT_TASK_MODE);
         }
 
         /**
@@ -5893,6 +5899,7 @@ public final class Settings {
             CLONE_TO_MANAGED_PROFILE.add(SOUND_EFFECTS_ENABLED);
             CLONE_TO_MANAGED_PROFILE.add(TEXT_SHOW_PASSWORD);
             CLONE_TO_MANAGED_PROFILE.add(TIME_12_24);
+            CLONE_TO_MANAGED_PROFILE.add(TRANSISTENT_TASK_MODE);
         }
 
         /** @hide */
